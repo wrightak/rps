dd9949354dd4a8a748a4d44148d686d2669cf468
Andrew Wright <anwright@pivotal.io>
2019-02-14 15:13:20 +0900

    PlayRoundRequest refactor

--- rps/src/rps.js
+++ rps/src/rps.js
@@ -1,17 +1,36 @@
 function Requests() {
     this.playRound = (p1Hand, p2Hand, observer) => {
-        if (!['rock', 'paper', 'scissors'].includes(p1Hand) ||
-            !['rock', 'paper', 'scissors'].includes(p2Hand)) {
+        new PlayRoundRequest(p1Hand, p2Hand, observer).process()
+    }
+}
+
+function PlayRoundRequest(p1Hand, p2Hand, observer) {
+    this.process = () => {
+        if (invalidThrow(p1Hand) || invalidThrow(p2Hand)) {
             observer.invalid()
-        } else if (p1Hand === p2Hand) {
+        } else if (tie()) {
             observer.tie()
-        } else if (p1Hand === 'scissors' && p2Hand === 'rock' ||
-            p1Hand === 'paper' && p2Hand === 'scissors' ||
-            p1Hand === 'rock' && p2Hand === 'paper') {
+        } else if (
+            p2Wins()
+        ) {
             observer.p2Wins()
         } else {
             observer.p1Wins()
         }
+    }
+
+    function tie() {
+        return p1Hand === p2Hand
+    }
+
+    function p2Wins() {
+        return p1Hand === 'scissors' && p2Hand === 'rock' ||
+            p1Hand === 'paper' && p2Hand === 'scissors' ||
+            p1Hand === 'rock' && p2Hand === 'paper'
+    }
+
+    function invalidThrow(hand) {
+        return !['rock', 'paper', 'scissors'].includes(hand)
     }
 }
 
